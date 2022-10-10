843a157e3ab17d41752f0e72c67c4ecd2293b808
seoyeon0413 <sally9476@naver.com>
2022-10-10 20:48:34 +0900

    initial commit

---
+++ src/main/java/com/notice/board/repository/BoardRepository.java
@@ -0,0 +1,10 @@
+package com.notice.board.repository;
+
+import com.notice.board.entity.Board;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BoardRepository extends JpaRepository<Board, Integer> {
+
+}
